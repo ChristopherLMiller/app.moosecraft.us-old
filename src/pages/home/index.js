8b193ac39b125b48d44cef6c2db1efea940845ba
moose51789 <moose51789@gmail.com>
2017-05-16 20:29:00 -0400

    title for home page

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
-
+import Helmet from 'react-helmet';
 import css from './home.scss';
 
 const Home = () => (
-  <div className={css.hero} />
+  <div className={css.hero}>
+    <Helmet>
+      <title>Home</title>
+    </Helmet>
+  </div>
 );
 
 export default Home;
