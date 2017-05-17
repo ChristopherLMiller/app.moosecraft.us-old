daa3989f4db2cbec9c107f44574f71e40b6b7095
moose51789 <moose51789@gmail.com>
2017-05-17 10:03:20 -0400

    copyright bar year dynamic

--- src/layout/copyright/index.js
+++ src/layout/copyright/index.js
@@ -4,7 +4,7 @@ import css from '../../styles/styles.scss';
 
 const Copyright = () => (
   <div className={css.copyright}>
-    <p>&copy; Copyright 2017 Moosecraft</p>
+    <p>&copy; Copyright {new Date().getFullYear()} Moosecraft</p>
   </div>
 );
 
